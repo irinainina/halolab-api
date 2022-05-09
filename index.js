d1d16a75e69d1ed3688e7e7729f7f9239bc9388a
Irina <irbris@mail.ru>
2022-05-09 19:19:49 +0300

    feat: update test api

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const cards = require('./data/cards');
 const users = require('./data/users');
 
 const app = express();
+app.use(express.static('styles'));
 app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-  res.write('Hello world');
-  res.end();
+  res.sendFile(createPath('index'));
 });
 
 app.get('/cards', (req, res) => {
