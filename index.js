a0ea9959cb37cf8cc0a980f99590fd9a4884b5ce
irinainina <irbris@mail.ru>
2022-05-09 13:51:02 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cards = require('./data/cards');
 const users = require('./data/users');
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
